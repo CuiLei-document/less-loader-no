f69023641c79d39c3e1d27186e1281c63d5b4a5b
lovelei <493114008@qq.com>
2021-03-24 17:54:46 +0800

    lessno

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,38 +1,37 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-    
-    entry:'./src/index.js',
-    output:{
-        filename: 'index.[contenthash].js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        title: 'cui',
-        template: 'src/assets/index.html'
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "cui",
+      template: "src/assets/index.html",
     }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        loader: ["style-loader", "css-loader", "less-loader"], // compiles Less to CSS
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
     ],
-    module:{
-        rules:[
-            {
-                test: /\.less$/i,
-                loader: ["style-loader","css-loader","less-loader"], // compiles Less to CSS
-              },
-            {
-                test: /\.scss$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require('dart-sass')
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-}
+  },
+};
